5853123f2693e822fb2c0b9a47ad6702ab6b8c0b
Hyun Joon Kim <hjkim74@myseneca.ca>
2025-04-25 23:19:31 -0400

    adding style.css and index.html and adding to app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from mma import get_fighters_stats, get_fighter_details
 
 app = Flask(__name__)
 
 fighters_list = get_fighters_stats()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/fighters', methods=['GET'])
 def get_fighters():
